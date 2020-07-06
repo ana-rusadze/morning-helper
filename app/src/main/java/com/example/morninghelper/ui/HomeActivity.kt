b609ed150d12c029b5ee06a70967dbbc95aa36f1
unknown <ana.rusadze.1@btu.edu.ge>
2020-07-06 23:35:45 +0400

    another try

--- app/src/main/java/com/example/morninghelper/ui/HomeActivity.kt
+++ app/src/main/java/com/example/morninghelper/ui/HomeActivity.kt
@@ -94,7 +94,7 @@ class HomeActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
         Log.d(DEBUG_TAG, "onSingleTapUp: $event")
         return true
     }
-//    blabla <-- Ulamazesi pasuxi
+//    Final 
 
 
 }
